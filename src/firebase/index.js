7d823a2e1235db8a36969ab86521ea81d1e832e1
kalanarathnayake <kalanagayanga8@gmail.com>
2023-05-11 22:56:18 +0530

    fix: firebase config fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,23 @@ import 'firebase/storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDL2EbrXYKEywOJofjMuon4Rh8D9UaUBtw",
-  authDomain: "imageupload-f5b47.firebaseapp.com",
-  projectId: "imageupload-f5b47",
-  storageBucket: "imageupload-f5b47.appspot.com",
-  messagingSenderId: "295986854046",
-  appId: "1:295986854046:web:2c6feb2181aa176cc4a38f",
-  measurementId: "G-V6JTV0ND3M"
+  // apiKey: "AIzaSyDL2EbrXYKEywOJofjMuon4Rh8D9UaUBtw",
+  // authDomain: "imageupload-f5b47.firebaseapp.com",
+  // projectId: "imageupload-f5b47",
+  // storageBucket: "imageupload-f5b47.appspot.com",
+  // messagingSenderId: "295986854046",
+  // appId: "1:295986854046:web:2c6feb2181aa176cc4a38f",
+  // measurementId: "G-V6JTV0ND3M"
+
+  apiKey: "AIzaSyBQZVCIoZuSOYVEjgTi5mgXGWfzqRyaQRo",
+  authDomain: "sondurumamal-263d6.firebaseapp.com",
+  projectId: "sondurumamal-263d6",
+  storageBucket: "sondurumamal-263d6.appspot.com",
+  messagingSenderId: "571352580422",
+  appId: "1:571352580422:web:a44e41a0667c175b59a547",
+  measurementId: "G-L961RTSJE8"
+
+
 };
 
 
@@ -18,4 +28,4 @@ firebase.initializeApp(firebaseConfig);
 
 const storage = firebase.storage();
 
-export { storage, firebase as default };
+export { storage, firebase as default };
